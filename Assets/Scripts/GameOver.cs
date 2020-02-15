e6d45139a4c504bd338f8cd429e65557bf5b8c69
unknown <fahmifayyadh>
2020-02-15 20:19:21 +0700

    finish game

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+
+    // Update is called once per frame
+
+    public void Play()
+    {
+            SceneManager.LoadScene("Gameplay ref");
+      
+    }
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/MunculObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MunculObject : MonoBehaviour
+{
+    public float jeda = 0.8f;
+    float timer;
+    public GameObject[] obyek;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > jeda)
+        {
+            int random = Random.Range(0, obyek.Length);
+            Instantiate(obyek[random], transform.position, transform.rotation);
+            timer = 0;
+        }
+    }
+}
